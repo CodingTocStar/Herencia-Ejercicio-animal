853ef858e3361badffa235ea58be704d9c08e926
gino <nahuel.dmz@outlook.com>
2023-06-29 22:00:54 -0300

    Animales terminado

---
+++ src/Main/Java2Encuentros19Al28Ejercicio1Animales.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author MiriamNahuel
+ */
+public class Java2Encuentros19Al28Ejercicio1Animales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
